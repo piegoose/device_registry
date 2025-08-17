8e6a9debdde609719c2b164205611a680f31ffa8
piegoose <jedrzej.sikora@puszkin.eu>
2025-08-17 14:01:44 +0200

    feat(service): implement AssignDeviceToUser with validation rules

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
 class AssignDeviceToUser
-  def initialize(requesting_user:, serial_number:, new_device_owner_id:)
+  def initialize(user:, device:)
+    @user = user
+    @device = device
   end
 
   def call
+    # 1. User attempts to assign another user's device → error
+    if @device.user_id.present? && @device.user_id != @user.id
+      raise AssigningError::AlreadyUsedOnOtherUser
+    end
+
+    # 2. If it is the same user, but they have already returned the device → error
+    if DeviceAssignment.exists?(user_id: @user.id, device_id: @device.id, returned: true)
+      raise AssigningError::AlreadyReturnedByThisUser
+    end
+
+    # 3. Assign the device to the user
+    DeviceAssignment.create!(user: @user, device: @device, returned: false)
   end
 end
